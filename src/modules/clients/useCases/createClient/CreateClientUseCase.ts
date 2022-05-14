52d893c20511b40c5887568d0fc25cd54d8d05f2
diego.czajka <diego.czajka@monditech.com.br>
2022-05-14 14:02:11 -0300

    create and authenticate user

---
+++ src/modules/clients/useCases/createClient/CreateClientUseCase.ts
@@ -0,0 +1,36 @@
+import { prisma } from "../../../../database/prismaClient";
+import { hash } from "bcrypt";
+
+interface ICreateClient {
+  username: string;
+  password: string;
+}
+
+export class CreateClientUseCase {
+  async execute({ password, username }: ICreateClient) {
+    //Validar se o usuário existe
+    const clientExists = await prisma.clients.findFirst({
+      where: {
+        username: {
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (clientExists) {
+      throw new Error("Client Already Exists!");
+    }
+
+    //criptografar a senha
+    const hashPasword = await hash(password, 10);
+
+    //Salvar o client
+    const client = await prisma.clients.create({
+      data: {
+        username,
+        password: hashPasword,
+      },
+    });
+    return client;
+  }
+}
