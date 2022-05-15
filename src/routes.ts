48df5b8a3ecafd1151287e1b9c7813637a090214
diego.czajka <diego.czajka@monditech.com.br>
2022-05-14 22:42:28 -0300

    creating deliverymans

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,24 @@
 import { Router } from "express";
 import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
+import { AuthenticateDeliverymanController } from "./modules/account/authenticateClient/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/createClientController";
+import { CreateDeliverymanController } from "./modules/deliveryman/useCases/CreateDeliverymanController";
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
-const authenticateClientController = new AuthenticateClientController()
+const authenticateClientController = new AuthenticateClientController();
+const createDeliverymanController = new CreateDeliverymanController();
+const authenticateDeliverymanController = new AuthenticateDeliverymanController();
 
+//clients
 routes.post("/client/", createClientController.handle);
-routes.post('/authenticate',authenticateClientController.handle)
+routes.post("/client/authenticate", authenticateClientController.handle);
+//deliveryman
+routes.post("/deliveryman", createDeliverymanController.handle);
+routes.post(
+  "/deliveryman/authenticate",
+  authenticateDeliverymanController.handle,
+);
 
 export { routes };
